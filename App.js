c998c91e36b44fd60be02df6a46d8f6bad4ffa71
Daniel Dimitrov <malkothuski@gmail.com>
2023-06-30 18:01:52 +0300

    add 3 jokers

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-
 import {Navigator} from './screens/MainTabScreen';
-import Tabs from './screens/TabNavigation/tabs';
 import {SafeAreaView} from 'react-native-safe-area-context';
-// import Tabs from './screens/TabNavigation/tabs';
 
 const Drawer = createDrawerNavigator();
 
